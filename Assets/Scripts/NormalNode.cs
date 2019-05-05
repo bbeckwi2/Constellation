34210d1a5bb2802e9da5c8ed9b4c3574dc918431
aowolfie <bluestar1996zz@gmail.com>
2019-05-05 15:06:22 -0400

    Added CSV stuff and created structs for node labels
    Added my half-baked CSV reader (Three projects running baby!)
    Added the data-set we will be working with.
    Created an enum for node types (Influence their shape and color!)
    Added a structure for node data.

--- Assets/Scripts/NormalNode.cs
+++ Assets/Scripts/NormalNode.cs
@@ -10,6 +10,7 @@ public class NormalNode : MonoBehaviour
 
     public GameObject connectionFab;
     private GameObject connection;
+
     public void init() {
         children = new List<NormalNode>();
         isInit = true;
@@ -17,14 +18,23 @@ public class NormalNode : MonoBehaviour
 
     public void addParent(NormalNode parent) {
         this.parent = parent;
+
+        // Connection fabrication
         connection = Instantiate(connectionFab);
+
+        // Positions
         Vector3 gPos = this.parent.gameObject.transform.position;
         Vector3 sPos = this.gameObject.transform.position;
+
+        // Get the transform and distance
         Transform t = connection.transform;
         float dist = Vector3.Distance(gPos, sPos);
+
+        // Get and set the link between the stars
         t.position = Vector3.Lerp(sPos, gPos, 0.5f);
         t.localScale = new Vector3(t.localScale.x, t.localScale.y, dist);
         t.LookAt(gPos);
+        
     }
 
     public void addChild(NormalNode child) {
